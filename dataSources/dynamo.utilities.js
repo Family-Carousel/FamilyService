db9b4eba1388763f71fc4177bd40699281ff6cec
Michael Cavanaugh <funkel1989@gmail.com>
2020-01-16 23:07:55 -0500

    dynamo changes

--- dataSources/dynamo.utilities.js
+++ dataSources/dynamo.utilities.js
@@ -3,104 +3,126 @@ const https = require('https');
 const AWS = require('aws-sdk');
 
 let awsConfig = {
-    region: 'us-east-2',
-    httpOptions: { agent: new https.Agent({ keepAlive: true, maxSockets: 50, rejectUnauthorized: true }) }
+  region: 'us-east-2',
+  httpOptions: { agent: new https.Agent({ keepAlive: true, maxSockets: 50, rejectUnauthorized: true }) }
 };
 
 const init = {
-    dynamodb: () => {
-        return new DynamoDB.DocumentClient(awsConfig);
-    }
+  dynamodb: () => {
+    return new DynamoDB.DocumentClient(awsConfig);
+  }
 };
 
+function paramsObjectFactory(tableName, hashName, hashValue, indexName = null, rangeName = null, rangeValue = null) {
+  let paramsObject = {
+    TableName: tableName,
+    KeyConditionExpression: `${hashName} = :hashKeyValue`,
+    ExpressionAttributeValues: {
+      ':hashKeyValue': hashValue
+    }
+  };
+
+  if (rangeName && rangeValue) {
+    paramsObject.KeyConditionExpression += ` and ${rangeName} = :rangeKeyValue`;
+    paramsObject.ExpressionAttributeValues = {
+      ':hashKeyValue': hashValue,
+      ':rangeKeyValue': rangeValue
+    };
+  }
+
+  if (indexName) paramsObject.IndexName = indexName;
+
+  return paramsObject;
+}
+
 module.exports = {
-    putObject: async (tableName, item) => {
-        return new Promise(function (resolve, reject) {
-            var docClient = init.dynamodb();
+  putObject: async (tableName, item) => {
+    return new Promise(function(resolve, reject) {
+      var docClient = init.dynamodb();
 
-            var params = {
-                TableName: tableName,
-                Item: item
-            };
-            console.log('table name: ', tableName);
-            console.log('item: ', item);
-            console.log('params: ', params);
+      var params = {
+        TableName: tableName,
+        Item: item
+      };
 
-            docClient.put(params).promise()
-                .then((res) => {
-                    console.log('dynamo res: ', res);
-                    console.log('resolve: ', resolve);
-                    return resolve(item);
-                })
-                .catch((err) => {
-                    console.log('dynamo err: ', err);
-                    console.log('reject: ', reject);
-                    return reject('Error putting document to dynamo: ' + err);
-                })
+      docClient
+        .put(params)
+        .promise()
+        .then(() => {
+          return resolve(item);
+        })
+        .catch((err) => {
+          return reject('Error putting document to dynamo. ' + err.message);
         });
-    },
-    list: async (tableName) => {
-        return new Promise(function (resolve, reject) {
-            var docClient = init.dynamodb();
+    });
+  },
 
-            var params = {
-                TableName: tableName
-            };
-
-            docClient.scan(params).promise()
-                .then((res) => {
-                    if (!res || !res.Items) {
-                        return reject('List did not return expected format: ' + JSON.stringify(res));
-                    }
-                    return resolve(res.Items);
-                })
-                .catch((err) => {
-                    return reject('Error getting list from dynamo: ' + err);
-                })
-        });
-    },
-    paramsObjectFactory: (tableName, indexName, attributeName, key) => {
+  batchPutObjects: async (tableName, itemList) => {
+    return new Promise(function(resolve, reject) {
+      let ddb = new DynamoDB(awsConfig);
+      let putRequests = itemList.map((item) => {
         return {
-            TableName: tableName,
-            indexName: indexName,
-            KeyConditionExpression: attributeName + ' = :hashKeyValue',
-            ExpressionAttributeValues: {
-                ':hashKeyValue': key
-            }
+          PutRequest: { Item: item }
         };
-    },
-    query: async (queryObj) => {
-        return new Promise(function (resolve, reject) {
-            var docClient = init.dynamodb();
+      });
 
-            docClient.query(queryObj).promise()
-                .then((res) => {
-                    return resolve(res.Items);
-                })
-                .catch((err) => {
-                    return reject('Error getting Query from dynamo: ' + err);
-                })
+      let params = {
+        RequestItems: {
+          [tableName]: putRequests
+        }
+      };
+
+      ddb
+        .batchWriteItem(params)
+        .promise()
+        .then(() => {
+          return resolve();
+        })
+        .catch((err) => {
+          return reject('Error putting list of documents(batch) to dynamo. ' + err.message);
         });
-    },
-    getByHashKey: async (tableName, keyObj) => {
-        return new Promise(function (resolve, reject) {
-            var docClient = init.dynamodb();
+    });
+  },
 
-            var params = {
-                TableName: tableName,
-                Key: keyObj
-            };
+  list: async (tableName) => {
+    return new Promise(function(resolve, reject) {
+      var docClient = init.dynamodb();
 
-            docClient.get(params).promise()
-                .then((res) => {
-                    if (!res || !res.Item) {
-                        return reject('Key does not exist in table: ' + JSON.stringify(res));
-                    }
-                    return resolve(res.Item);
-                })
-                .catch((err) => {
-                    return reject('Error getting Document from dynamo: ' + err);
-                })
+      var params = {
+        TableName: tableName
+      };
+
+      docClient
+        .scan(params)
+        .promise()
+        .then((res) => {
+          if (!res || !res.Items) {
+            return reject('List did not return in expected format: ' + JSON.stringify(res));
+          }
+          return resolve(res.Items);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          return reject('Error getting document from dynamo. ' + err.message);
         });
-    },
+    });
+  },
+
+  query: async (tableName, hashName, hashValue, indexName = null, rangeName = null, rangeValue = null) => {
+    const queryObj = paramsObjectFactory(tableName, hashName, hashValue, indexName, rangeName, rangeValue);
+    console.log('tableName', tableName);
+    return new Promise(function(resolve, reject) {
+      var docClient = init.dynamodb();
+
+      docClient
+        .query(queryObj)
+        .promise()
+        .then((res) => {
+          return resolve(res.Items);
+        })
+        .catch((err) => {
+          return reject('Error querying dynamo. ' + err.message);
+        });
+    });
+  }
 };
---
+++ dataSources/family-repository.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const dynamoUtils = require('./dynamo.utilities');
+const tableName = process.env.FAMILY_TABLE;
+const _ = require('lodash');
+
+// TODO: Convert for batch put of family's if ever needed
+// function buildPutRequestItemListOfTemplates(templateItemList, newTemplateName) {
+//     let updatedTemplates = _.map(templateItemList, (t) => {
+//       let newPutRequestObject = {
+//         LastUpdateBy: { S: t.LastUpdateBy.toString() },
+//         IsTrackingPixel: { N: t.IsTrackingPixel.toString() },
+//         IsActive: { N: t.IsActive.toString() },
+//         IsResendable: { N: t.IsResendable.toString() },
+//         CreatedDateTime: { S: t.CreatedDateTime },
+//         VersionId: { S: t.VersionId },
+//         LastUpdateDateTime: { S: t.LastUpdateDateTime },
+//         CreatedBy: { S: t.CreatedBy.toString() },
+//         Id: { S: t.Id },
+//         IsOnDoNotSendList: { N: t.IsOnDoNotSendList.toString() },
+//         ApplicationId: { S: t.ApplicationId },
+//         DefaultEmailAddresses: { M: { From: { S: t.DefaultEmailAddresses.From }, Bcc: { SS: t.DefaultEmailAddresses.Bcc }, Cc: { SS: t.DefaultEmailAddresses.Cc }, To: { SS: t.DefaultEmailAddresses.To } } }
+//       };
+  
+//       if (t.Path) {
+//         newPutRequestObject.Path = { S: t.Path };
+//       }
+  
+//       if (t.Stages && t.Stages.length) {
+//         newPutRequestObject.Stages = { SS: t.Stages };
+//       }
+  
+//       if (t.Description) {
+//         newPutRequestObject.Description = { S: t.Description };
+//       }
+  
+//       if (t.Subject) {
+//         newPutRequestObject.Subject = { S: t.Subject };
+//       }
+  
+//       if (newTemplateName !== t.Name) {
+//         newPutRequestObject.Name = { S: newTemplateName };
+//       } else {
+//         newPutRequestObject.Name = { S: t.Name };
+//       }
+//       console.log('nameUpdateNewPutRequestObject', JSON.stringify(newPutRequestObject));
+  
+//       return newPutRequestObject;
+//     });
+  
+//     return updatedTemplates;
+//   }
+
+module.exports = {
+    saveFamily: async (familyData) => {
+        try {
+            console.log('we made it to the dynamo put');
+            const returnValue = await dynamoUtils.putObject(tableName, familyData);
+            console.log('returnValue: ', returnValue);
+            return returnValue;
+        } catch (err) {
+            console.error('Error updating family via Dynamo: ' + err);
+            throw('Error updating family via Dynamo');
+        }
+    },
+    getFamilyById: async (FamilyId) => {
+        try {
+            return await dynamoUtils.getByHashKey(tableName, { FamilyId: FamilyId });
+        } catch(err) {
+            console.error('Error getting family by id via Dynamo: ' + err);
+            throw('Error getting family');
+        }
+    },
+    listFamilysByMemberId: async (memberId) => {
+        try {
+            let qobj = dynamoUtils.paramsObjectFactory(tableName, "MemberId_IDX", "MemberId", memberId);
+            return await dynamoUtils.query(qobj);
+        } catch(err) {
+            console.error('Error listing familys by memberId via Dynamo: ' + err);
+            throw('Error listing familys by memberId');
+        }
+    }
+};
