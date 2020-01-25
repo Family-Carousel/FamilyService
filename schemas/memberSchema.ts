5946c88b53466f65a83eb03a25db062c3ac1df33
Michael Cavanaugh <funkel1989@gmail.com>
2020-01-24 22:42:08 -0500

    member validation schema

---
+++ schemas/memberSchema.ts
@@ -0,0 +1,28 @@
+import { createSchema as S, TsjsonParser } from 'ts-json-validator';
+import { IMember } from '../interfaces/IMember';
+
+const parser = new TsjsonParser(S({
+    type: 'object',
+    properties: {
+        Id: S({ type: 'string' }),
+        FirstName: S({ type: 'string' }),
+        LastName: S({type: 'string'}),
+        DateOfBirth: S({type: 'string'}),
+        ManagedUser: S({type: 'number'}),
+        EmailAddress: S({type: 'string'}),
+        Age: S({type: 'number'}),
+        CreateBy: S({type: 'string'}),
+        CreateDateTime: S({type: 'string'}),
+        LastUpdateBy: S({type: 'string'}),
+        LastUpdateDateTime: S({type: 'string'})
+    },
+    required: ['Id', 'FirstName', 'DateOfBirth', 'ManagedUser', 'EmailAddress', 'CreateBy', 'CreateDateTime', 'LastUpdateBy', 'LastUpdateDateTime']
+}))
+
+export function caseTsJsonValidator(data: IMember) {
+    if (parser.validates(data)) {
+        return data;
+    }
+
+    throw new Error(`Invalid ${JSON.stringify(parser.getErrors())}`);
+}
