caca451adb6a338015df86a8937e8abcd0647d8c
Michael Cavanaugh <funkel1989@gmail.com>
2020-02-13 22:29:17 -0500

    trying out DI on handler with classes

--- handlers/Create.ts
+++ handlers/Create.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import * as sourceMaps from 'source-map-support';
 import { APIGatewayEvent, ProxyResult } from 'aws-lambda';
+import { inject } from 'inversify';
 sourceMaps.install();
 
 import DIContainer from '../di-container';
@@ -8,47 +9,54 @@ import { utilities } from './utilities';
 import { FamilyService } from '../services/FamilyService';
 import { MemberService } from '../services/MemberService';
 
-const familyService: FamilyService = DIContainer.resolve<FamilyService>(FamilyService);
-const memberService: MemberService = DIContainer.resolve<MemberService>(MemberService);
+export class CreateHandler {
+    private _familyService: FamilyService;
+    private _memberService: MemberService;
 
-export const createFamily = async (
-    event: APIGatewayEvent,
-): Promise<ProxyResult> => {
-    try {
-        if (!event.body) {
-            return utilities.BuildResponse(400, JSON.stringify('Object to create was not provided'));
+    constructor(@inject(FamilyService) familyService?: FamilyService, @inject(MemberService) memberService?: MemberService) {
+        this._familyService = familyService || DIContainer.resolve<FamilyService>(FamilyService);
+        this._memberService = memberService || DIContainer.resolve<MemberService>(MemberService);
+    }
+
+    public async CreateFamily(event: APIGatewayEvent): Promise<ProxyResult> {
+        try {
+            if (!event.body) {
+                return utilities.BuildResponse(400, JSON.stringify('Object to create was not provided'));
+            }
+            const familyData = JSON.parse(event.body);
+            const familyServerReturn = await this._familyService.createFamily(familyData);
+    
+            if (!familyServerReturn) {
+                return utilities.BuildResponse(404, JSON.stringify('Failed to create Family'));
+            }
+    
+            return utilities.BuildResponse(201, JSON.stringify(familyServerReturn));
+        } catch(err) {
+            console.error('Family Service Create a family error: ', err);
+            return utilities.BuildResponse(500, JSON.stringify('Family Service internal server error'));
         }
-        const familyData = JSON.parse(event.body);
-        const familyServerReturn = await familyService.createFamily(familyData);
+    }
 
-        if (!familyServerReturn) {
-            return utilities.BuildResponse(404, JSON.stringify('Failed to create Family'));
+    public async CreateMember(event: APIGatewayEvent): Promise<ProxyResult> {
+        try {
+            if (!event.body) {
+                return utilities.BuildResponse(400, JSON.stringify('Object to create was not provided'));
+            }
+            const memberData = JSON.parse(event.body);
+            const memberServerReturn = await this._memberService.createMember(memberData);
+    
+            if (!memberServerReturn) {
+                return utilities.BuildResponse(404, JSON.stringify('Failed to create Member'));
+            }
+    
+            return utilities.BuildResponse(201, JSON.stringify(memberServerReturn));
+        } catch(err) {
+            console.error('Family Service Create a member error: ', err);
+            return utilities.BuildResponse(500, JSON.stringify('Family Service internal server error'));
         }
-
-        return utilities.BuildResponse(201, JSON.stringify(familyServerReturn));
-    } catch(err) {
-        console.error('Family Service Create a family error: ', err);
-        return utilities.BuildResponse(500, JSON.stringify('Family Service internal server error'));
     }
 }
 
-export const createMember = async (
-    event: APIGatewayEvent,
-): Promise<ProxyResult> => {
-    try {
-        if (!event.body) {
-            return utilities.BuildResponse(400, JSON.stringify('Object to create was not provided'));
-        }
-        const memberData = JSON.parse(event.body);
-        const memberServerReturn = await memberService.createMember(memberData);
-
-        if (!memberServerReturn) {
-            return utilities.BuildResponse(404, JSON.stringify('Failed to create Member'));
-        }
-
-        return utilities.BuildResponse(201, JSON.stringify(memberServerReturn));
-    } catch(err) {
-        console.error('Family Service Create a member error: ', err);
-        return utilities.BuildResponse(500, JSON.stringify('Family Service internal server error'));
-    }
-}
+export const invokeCreateHandler = new CreateHandler();
+export const createFamily = invokeCreateHandler.CreateFamily.bind(invokeCreateHandler);
+export const createMember = invokeCreateHandler.CreateMember.bind(invokeCreateHandler);
