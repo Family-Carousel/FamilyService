aecb4e6638e818301d99c5ac263c9a151a2bfe40
Michael Cavanaugh <funkel1989@gmail.com>
2020-03-16 21:01:40 -0400

    formatting

--- di-container.ts
+++ di-container.ts
@@ -1,16 +1,21 @@
 import { Container } from 'inversify';
+
 import { FamilyRepo } from './dataSources/family-repository';
 import { MemberRepo } from './dataSources/member-repository';
+
 import { FamilyService } from './services/FamilyService';
 import { MemberService } from './services/MemberService';
+
 import { DynamoUtilities } from './dataSources/dynamo.utilities';
 
 var DIContainer = new Container();
 
 DIContainer.bind<FamilyRepo>(FamilyRepo).toSelf();
 DIContainer.bind<MemberRepo>(MemberRepo).toSelf();
+
 DIContainer.bind<FamilyService>(FamilyService).toSelf();
 DIContainer.bind<MemberService>(MemberService).toSelf();
+
 DIContainer.bind<DynamoUtilities>(DynamoUtilities).toSelf();
 
 export default DIContainer;
