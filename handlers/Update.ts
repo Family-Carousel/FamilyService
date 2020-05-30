8fc903ba03d6a3325e26855d70853ec72ad6032a
Michael Cavanaugh <funkel1989@gmail.com>
2020-05-30 16:32:34 -0400

    calendar event is now updated

--- handlers/Update.ts
+++ handlers/Update.ts
@@ -9,14 +9,18 @@ import DIContainer from '../di-container';
 import { Utilities } from './utilities';
 import { FamilyService } from '../services/FamilyService';
 import { MemberService } from '../services/MemberService';
+import { CalendarService } from '../services/CalendarService';
 
 export class UpdateHandler {
     private _familyService: FamilyService;
     private _memberService: MemberService;
+    private _calendarService: CalendarService
 
-    constructor(@inject(FamilyService) familyService?: FamilyService, @inject(MemberService) memberService?: MemberService) {
+    constructor(@inject(FamilyService) familyService?: FamilyService, @inject(MemberService) memberService?: MemberService, 
+    @inject(CalendarService) calendarService?: CalendarService) {
         this._familyService = familyService || DIContainer.resolve<FamilyService>(FamilyService);
         this._memberService = memberService || DIContainer.resolve<MemberService>(MemberService);
+        this._calendarService = calendarService || DIContainer.resolve<CalendarService>(CalendarService);
     }
 
     public async UpdateFamily(event: APIGatewayEvent): Promise<ProxyResult> {
@@ -101,6 +105,51 @@ export class UpdateHandler {
         } 
     }
 
+    public async UpdateCalendarEventForFamily(event: APIGatewayEvent): Promise<ProxyResult> {
+        try {
+            if (!event || !event.pathParameters || !event.pathParameters.familyId) {
+                return Utilities.BuildResponse(400, JSON.stringify('familyId is required in path to perform update'));
+            }
+    
+            if (!event.pathParameters.id) {
+                return Utilities.BuildResponse(400, JSON.stringify('id is required in path to perform update'));
+            }
+    
+            if (!event.body) {
+                return Utilities.BuildResponse(400, JSON.stringify('Update body must be included'));
+            }
+    
+            const id = event.pathParameters.id;
+            const familyId = event.pathParameters.familyId
+            let newEventData = JSON.parse(event.body);
+    
+            if (familyId !== newEventData.FamilyId ) {
+                return Utilities.BuildResponse(400, JSON.stringify('Id mismatch. FamilyId in Body must match familyId in Path'));
+            }
+    
+            if (id !== newEventData.Id ) {
+                return Utilities.BuildResponse(400, JSON.stringify('Id mismatch. Id in Body must match id in Path'));
+            }
+    
+            let currentEvent = await this._calendarService.GetCalendarEventByCompositKey(familyId, id);
+    
+            if (!currentEvent) {
+                return Utilities.BuildResponse(404, JSON.stringify('Calendar Event does not exist. Is your Id wrong?'));
+            }
+    
+            const updatedEvent = await this._calendarService.UpdateCalenderEvent(currentEvent, newEventData);
+    
+            if (!updatedEvent) {
+                return Utilities.BuildResponse(404, JSON.stringify('Calendar Event update not successful'));
+            }
+    
+            return Utilities.BuildResponse(200, JSON.stringify(updatedEvent));
+        } catch (err) {
+            console.error('Family Service Calendar Event update failure: ' + err);
+            return Utilities.BuildResponse(500, 'Family Service Calendar Event update failure');
+        } 
+    }
+
     public async UpdateMemberGlobally(event: APIGatewayEvent): Promise<ProxyResult> {
         try {
             if (!event || !event.pathParameters || !event.pathParameters.id) {
@@ -142,3 +191,4 @@ export const invokeUpdateHandler = new UpdateHandler();
 export const updateFamily = invokeUpdateHandler.UpdateFamily.bind(invokeUpdateHandler);
 export const updateMemberForFamily = invokeUpdateHandler.UpdateMemberForFamily.bind(invokeUpdateHandler);
 export const updateMemberGlobally = invokeUpdateHandler.UpdateMemberGlobally.bind(invokeUpdateHandler);
+export const updateCalendarEventForFamily = invokeUpdateHandler.UpdateCalendarEventForFamily.bind(invokeUpdateHandler);
